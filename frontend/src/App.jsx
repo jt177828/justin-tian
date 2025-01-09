d838f376550211963a408e7505afc0d67ad1bc2f
Justin Tian <justin.tian.372@gmail.com>
2025-01-09 12:42:42 -0500

    add vercel speed insights package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +12,7 @@ function App() {
       <Sidebar></Sidebar>
 
       <Container></Container>
+      <SpeedInsights/>
     </div>
   );
 }
